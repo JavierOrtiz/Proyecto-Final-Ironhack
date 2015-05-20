bdb967f5a67ead120f95845ec2659a436def4e6d
Javier Ortiz <zonabetaes@gmail.com>
2015-05-20 10:30:22 +0200

    Añadida tabla de relaccion intermedia

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ActiveRecord::Base
-    belongs_to :user
+#    belongs_to :user
+    has_many :employees, :foreign_key => 'user_id', :class_name => "User"
+    belongs_to :boss, inverse_of: :employees, :class_name => "User", :foreign_key => 'user_id'
 end
