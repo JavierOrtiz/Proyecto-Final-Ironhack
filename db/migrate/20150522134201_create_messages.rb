bd469491bb924d87fa30525f02548cf9ccc3d3d9
Javier Ortiz <zonabetaes@gmail.com>
2015-05-22 15:46:06 +0200

    Migracion messages preparada

--- db/migrate/20150522134201_create_messages.rb
+++ db/migrate/20150522134201_create_messages.rb
@@ -1,7 +1,12 @@
 class CreateMessages < ActiveRecord::Migration
   def change
     create_table :messages do |t|
-
+        t.belongs_to :user, index: true
+        t.date :date_send
+        t.string :status
+        t.string :title
+        t.text :content
+        t.integer :to
       t.timestamps null: false
     end
   end
